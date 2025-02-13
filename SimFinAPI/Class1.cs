9fbeaec8047ecb7dbc10b0055e86dae0612b727f
Alex Sheets <asheet3@lsu.edu>
2025-02-13 10:49:58 -0600

    Add project files.

---
+++ SimFinAPI/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+
+// https://www.milanjovanovic.tech/blog/the-right-way-to-use-httpclient-in-dotnet
+
+namespace SimFinAPI
+{
+    public class Class1
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public async Task<JsonContent> RequestCompanyStatements(string ticker, string statement_type, int year, string quarter)
+        {
+            // instantiate client
+            using var client = new HttpClient();
+
+        }
+    }
+}
